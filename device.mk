f3d3d238a121520dbc9775c8ccd7e4c0ee1625c2
sm6150-dreams <rn10prodev@gmail.com>
2026-08-04 17:07:14 +0000

    klee: switch to LunarisDolby
    Signed-off-by: sm6150-dreams <rn10prodev@gmail.com>

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     MtkInCallService \
-    DolbyAtmos
+    LunarisDolby
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_config.xml \
